8cdc5d832dd211cd50b8b901b78c0faf9747a43d
Nalam Siva Samanth Kishor Gupta <nalamsiva882@gmail.com>
2026-09-21 10:34:26 +0530

    create.sql

--- create.sql
+++ create.sql
@@ -48,3 +48,63 @@ INSERT INTO employees
 (employee_id, name, email, salary, department_id)
 VALUES
 (101, 'Rahul', 'rahul@gmail.com', 50000, 1);
+
+ Problem Statement : A college want to maintain info about it scores and students create a database name college _db and create the following tables 
+
+courses: 
+
+column  	Datatype	constraint
+course_id       INT              PRIMARY KEY
+course_name     Varchar(100)     UNIQUE, NOT NULL
+duration        INT 		 CHECK
+fees 		DECIMAL(10,2)    CHECK
+
+
+STUDENTS:
+
+column 		Datatype 	constarint 
+student_id 	INT             PRIMARY KEY 
+student_name    Varchar(100)    NOT NULL
+email 		Varchar(100)    UNIQUE
+age             INT    		CHECK
+city            Varchar(50)     DEFAULT
+course_id	INT		FOREIGN KEY
+
+
+Queies:
+
+USE college_db;
+CREATE TABLE courses (
+    course_id INT PRIMARY KEY,
+    course_name VARCHAR(100) UNIQUE NOT NULL,
+    duration INT CHECK (duration > 0),
+    fees DECIMAL(10,2) CHECK (fees >= 0)
+);
+
+CREATE TABLE students (
+    student_id INT PRIMARY KEY,
+    student_name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE,
+    age INT CHECK (age >= 18),
+    city VARCHAR(50) DEFAULT 'Guntur',
+    course_id INT,
+    
+    FOREIGN KEY (course_id) REFERENCES courses(course_id)
+);
+
+INSERT INTO courses (course_id, course_name, duration, fees)
+VALUES
+(101, 'Python', 6, 15000.00),
+(102, 'Java', 8, 20000.00),
+(103, 'Data Science', 10, 30000.00),
+(104, 'Web Development', 6, 18000.00),
+(105, 'Machine Learning', 12, 40000.00);
+
+INSERT INTO students 
+(student_id, student_name, email, age, city, course_id)
+VALUES
+(1, 'Kishor', 'kishor@gmail.com', 21, 'Guntur', 101),
+(2, 'Rahul', 'rahul@gmail.com', 22, 'Vijayawada', 102),
+(3, 'Priya', 'priya@gmail.com', 20, 'Hyderabad', 103),
+(4, 'Anil', 'anil@gmail.com', 23, 'Guntur', 104),
+(5, 'Sneha', 'sneha@gmail.com', 21, 'Chennai', 105);
